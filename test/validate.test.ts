f60c3426758803ac462811984b8430491ea33f9c
Roderik van der Veer <roderik@settlemint.com>
2018-11-28 20:31:37 +0400

    fix: more test fixes

--- test/validate.test.ts
+++ test/validate.test.ts
@@ -1,8 +1,6 @@
 import { CertiMintValidation } from '../src/validate';
 
-describe('certiMintValidation', () => {
-  describe('Initialise the class', () => {
-    const certiMintValidation = new CertiMintValidation();
-    expect(certiMintValidation).toBeTruthy();
-  });
+test('Initialise the class', () => {
+  const certiMintValidation = new CertiMintValidation();
+  expect(certiMintValidation).toBeTruthy();
 });
